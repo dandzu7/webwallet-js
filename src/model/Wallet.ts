c7e6946a2b63f67e1bdbf3ad3a2f68b087ccfcea
dandzu7 <166304612+dandzu7@users.noreply.github.com>
2024-04-10 19:10:58 +0700

    Update Wallet.ts

--- src/model/Wallet.ts
+++ src/model/Wallet.ts
@@ -47,7 +47,7 @@ export class WalletOptions {
 	checkMinerTx: boolean = false;
 	readSpeed: number = 10;
 	customNode: boolean = false;
-	nodeUrl: string = 'https://pr02.myqwertycoin.com/sync/';
+	nodeUrl: string = 'http://139.180.184.63:8197/';
 
 	static fromRaw(raw: RawWalletOptions) {
 		let options = new WalletOptions();
